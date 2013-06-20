10a6eb642ee73d5f3d7e0423d0ebe264307d9687
Lucjan Suski <lucjansuski@gmail.com>
2013-06-20 13:40:31 +0200

    Added page images, fixed wydarzylo sie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :promoted
-
+  
   def promoted
     @promoted = Refinery::Events::Event.promoted
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def page_image
+    if @page
+      if @page.parent
+        @page.parent.slug
+      else
+        @page.slug
+      end
+    elsif params[:type]
+      params[:type].parameterize
+    else
+      'top-foto'
+    end
+  end
 end
