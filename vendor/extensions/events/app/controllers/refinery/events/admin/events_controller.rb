c43a54513f756bcadfee98540b5c52e741aafff6
Lucjan Suski <lucjansuski@gmail.com>
2013-09-21 12:04:43 +0200

    Added types selection in events admin

--- vendor/extensions/events/app/controllers/refinery/events/admin/events_controller.rb
+++ vendor/extensions/events/app/controllers/refinery/events/admin/events_controller.rb
@@ -3,9 +3,23 @@ module Refinery
     module Admin
       class EventsController < ::Refinery::AdminController
 
+        before_filter :types
+
         crudify :'refinery/events/event',
-                :title_attribute => 'name',
-                :per_page => 100000
+                :title_attribute => 'name'
+
+        def index
+          @events = Refinery::Events::Event.order('refinery_events.position asc')
+          @events = @events.by_type(params[:type]) if params[:type]
+        end
+
+        def self.pageable?
+          false
+        end
+
+        def types
+          @types = Refinery::Events::Type.all
+        end
 
       end
     end
