885b601abd3dd1c08da503fcccf822f37d165a7c
Lucjan Suski <lucjansuski@gmail.com>
2014-09-16 13:26:18 +0200

    fix styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,16 +26,16 @@ custom_wymeditor_boot_options = {classesItems: []}
 
 $(function() {
 
-    var marquee = $(".marquee"); 
+    var marquee = $(".marquee");
     marquee.css({"overflow": "hidden", "width": "100%"});
 
     // wrap "My Text" with a span (IE doesn't like divs inline-block)
     marquee.wrapInner("<span>");
-    marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" }); 
+    marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" });
     marquee.append(marquee.find("span").clone()); // now there are two spans with "My Text"
 
     marquee.wrapInner("<div>");
-    marquee.find("div").css("width", "200%");
+    marquee.find("div").css("width", "300%");
 
     var reset = function() {
         $(this).css("margin-left", "0%");
